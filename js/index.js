86e0d906b77af505029feb3e359a726248e8b110
Mahbub-web-hero-1997 <mahbub.programing.hero@gmail.com>
2022-02-17 07:23:16 +0600

    new added

--- js/index.js
+++ js/index.js
@@ -7,18 +7,20 @@ function expenses(expenseId) {
 // expenses calculation
 document.getElementById('calculate_Button').addEventListener('click', function () {
     const foodExpenses = expenses('food_Expense');
-    const rentExpenses = expenses('rent_Expense')
-    const clothesExpenses = expenses('clothes_Expense')
+    const rentExpenses = expenses('rent_Expense');
+    const clothesExpenses = expenses('clothes_Expense');
     const income = expenses('income');
     const totalExpenses = document.getElementById('expenses_Total').innerText = foodExpenses + rentExpenses + clothesExpenses;
     const balance = document.getElementById('balance').innerText = income - totalExpenses;
-    console.log('balance.........', balance)
+    if (income < 0 || foodExpenses < 0 || rentExpenses < 0 || clothesExpenses < 0) {
+        alert('Plese Enter Posetive Value')
+    }
 })
 // Bonus Aria
 document.getElementById('savingButton').addEventListener('click', function () {
     const parcentInput = expenses('parcent_input');
     const income = expenses('income');
-    const savingTotal = document.getElementById('saving_Amount').innerText = income * parcentInput / 100;
     const balancee = document.getElementById('balance').innerText;
+    const savingTotal = document.getElementById('saving_Amount').innerText = income * parcentInput / 100;
     const subTotal = document.getElementById('subTotal').innerText = balancee - savingTotal;
 })
